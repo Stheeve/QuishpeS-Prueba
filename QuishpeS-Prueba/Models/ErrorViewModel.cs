9acf233f0b9753d939f2df9bf0d6dcfbbcd212c3
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-03-27 08:32:52 -0500

    Agregar archivos de proyecto.

---
+++ QuishpeS-Prueba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuishpeS_Prueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
